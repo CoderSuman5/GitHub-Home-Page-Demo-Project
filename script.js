16bfd331ac5acc5fb8fc485a9037ccf426e75d38
suman biswas <satyajeetbishwash162087@gmail.com>
2025-01-06 18:05:43 +0530

    removed unessecary codes

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("GitHub home page loaded!");
-// });
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("Page loaded successfully!");
-// });
-
 
 
 document.addEventListener("DOMContentLoaded", () => {
