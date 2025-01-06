77de1f4be596244720adf1d923758e747e6f91f1
suman biswas <satyajeetbishwash162087@gmail.com>
2025-01-06 18:00:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("GitHub home page loaded!");
+// });
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("Page loaded successfully!");
+// });
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("GitHub Clone Loaded!");
+});
